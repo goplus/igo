be510aacc96ea445bfea7c45da65af16282a1f7e
xushiwei <x@goplus.org>
2021-10-07 14:57:02 +0800

    igo exec

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/goplus/igo
+
+go 1.16
+
+require github.com/qiniu/x v1.11.5
---
+++ internal/reflectx/reflectx.go
@@ -0,0 +1,11 @@
+package reflectx
+
+import "reflect"
+
+func Field(s reflect.Value, i int) reflect.Value {
+	panic("todo")
+}
+
+func FieldByIndex(s reflect.Value, index []int) reflect.Value {
+	panic("todo")
+}
