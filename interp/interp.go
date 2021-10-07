2c85d36a57728b598ab04b189c69149c9c979b61
xushiwei <x@goplus.org>
2021-10-07 19:05:08 +0800

    github.com/goplus/igo/interp

---
+++ interp/interp.go
@@ -0,0 +1,44 @@
+package interp
+
+import (
+	"go/ast"
+	"go/token"
+
+	"github.com/goplus/igo/internal/cl"
+	exec "github.com/goplus/igo/internal/exec/bytecode"
+)
+
+func init() {
+	cl.CallBuiltinOp = exec.CallBuiltinOp
+}
+
+// -----------------------------------------------------------------------------
+
+type Program exec.Code
+
+// CompileAST compiles a package by specified AST.
+func CompileAST(pkg *ast.Package, fset *token.FileSet) (app *Program, err error) {
+	b := exec.NewBuilder(nil)
+	_, err = cl.NewPackage(b.Interface(), pkg, fset, cl.PkgActClMain)
+	if err != nil {
+		return
+	}
+	code := b.Resolve()
+	return (*Program)(code), nil
+}
+
+// -----------------------------------------------------------------------------
+
+type Runtime struct {
+}
+
+func New() *Runtime {
+	return &Runtime{}
+}
+
+func (p *Runtime) RunProgram(app *Program) {
+	ctx := exec.NewContext((*exec.Code)(app))
+	ctx.Run()
+}
+
+// -----------------------------------------------------------------------------
